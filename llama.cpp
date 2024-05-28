db932a5df8e8ca5d3ca04760640e4ab1029fd7a0
Pavel Fatin <mail@pavelfatin.com>
2024-05-28 19:42:04 +0200

    misc fixes

--- llama.cpp
+++ llama.cpp
@@ -1666,7 +1666,6 @@ struct llama_anonymous_mmap : llama_mmap {
         align_to_next_page(&first, dwPageSize);
         align_to_previous_page(&last, dwPageSize);
 
-#if _WIN32_WINNT >= 0x603
         DWORD (WINAPI *pOfferVirtualMemory) (PVOID, SIZE_T, DWORD);
         HMODULE hKernel32 = GetModuleHandleW(L"kernel32.dll");
 
@@ -1676,17 +1675,18 @@ struct llama_anonymous_mmap : llama_mmap {
             if (pOfferVirtualMemory((char *) addr + first, last - first, 0x00000004 /* VMOfferPriorityNormal */)) {
                 LLAMA_LOG_WARN("warning: OfferVirtualMemory failed: %s\n", llama_format_win_err(GetLastError()).c_str());
             }
+        } else {
+            LLAMA_LOG_WARN("warning: OfferVirtualMemory unavailable: %s\n", llama_format_win_err(GetLastError()).c_str());
+            if (VirtualAlloc((char *) addr + first, last - first, MEM_RESET, 0)) {
+                LLAMA_LOG_WARN("warning: VirtualAlloc(.., MEM_RESET) failed: %s\n", llama_format_win_err(GetLastError()).c_str());
+            }
         }
-#else
-        if (VirtualAlloc((char *) addr + first, last - first, MEM_RESET, PAGE_NOACCESS)) {
-            LLAMA_LOG_WARN("warning: VirtualAlloc(.., MEM_RESET) failed: %s\n", llama_format_win_err(GetLastError()).c_str());
-        }
-#endif
     }
 
 #else
-    llama_anonymous_mmap(struct llama_file * file) {
+    llama_anonymous_mmap(struct llama_file * file, bool prefetch) {
         GGML_UNUSED(file);
+        GGML_UNUSED(prefetch);
 
         throw std::runtime_error("mmap not supported");
     }
